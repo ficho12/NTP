abe06c34f1c43a85ef3357ef29f1ae9dfee9375c
Joaquin Furones <JoaquinFurones@usal.es>
2022-04-14 00:51:51 +0200

    Añadida funcionalidad de eleccion de par basica

---
+++ src/clases/Par.java
@@ -0,0 +1,13 @@
+package clases;
+
+public class Par {
+
+	public long offset;
+	public long delay;
+	
+	public Par(long o, long d) {
+		this.offset=o;
+		this.delay=d;
+	}
+	
+}
--- src/servicios/Cliente.java
+++ src/servicios/Cliente.java
@@ -10,27 +10,30 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 
+import clases.Par;
+
 public class Cliente {
 
 	public static void main(String[] args) {
 		
 		int numServidores = args.length;
-		ArrayList<Integer> IP = new ArrayList<Integer>();
-		long t0,t1,t2,t3;
+		ArrayList<String> IP = new ArrayList<String>();
+		long t0=0,t1=0,t2=0,t3=0;
 		String respuesta;
 		String separado[];
+		ArrayList<Par> pares;
 		
-		if(args.length<2 || args.length>4)
+		if(args.length<1 || args.length>3)
 		{
 			System.out.println("Error.\n\n"
-					+ "Uso: Introducir tantos parametros como IPs se quieran usar\n");
+					+"Uso: Introducir tantos parametros como IPs se quieran usar\n");
 			return; 
 		}
 		
 		for(int i = 0; i < numServidores; i++)
 		{
 			try {
-				IP.add(Integer.parseInt(args[i]));
+				IP.add(args[i]);
 			} catch (NumberFormatException e) {
 				e.printStackTrace();
 				System.out.println("\n\nError 1: Introduzca IPs válidas, no: " + args[i] +"\n\n"
@@ -45,9 +48,12 @@ public class Cliente {
 		for(int i = 0; i < numServidores; i++)		//Para cada máquina
 		{
 			Client client=ClientBuilder.newClient();
-			URI uri=UriBuilder.fromUri("http://" + IP.get(i) + ":8080/Servidor/").build();	//"http://localhost:8080/Servidor/"
-			WebTarget target = client.target(uri);											//"http://" + Ip + ":8080/Servidor/"
+			System.out.println(IP.get(i));
+			URI uri=UriBuilder.fromUri("http://" + IP.get(i) + ":8080/NTP/").build();	//"http://localhost:8080/NTP/"
+			WebTarget target = client.target(uri);										//"http://" + Ip + ":8080/Servidor/"
 			
+			pares = new ArrayList<Par>();
+			Par mejorPar=null;
 			for(int j = 0; j<8; j++)				//8 repeticiones
 			{
 
@@ -59,9 +65,11 @@ public class Cliente {
 					d <- determinarDelay (t0,t1,t2,t3)
 					si ( d < mejorPar.d ) mejorPar <- {d,o}
 				 */
+				mejorPar = new Par(9999,9999);
+				
 				t0 = System.nanoTime();
 
-				respuesta = target.path("Carrera100/pedirTiempo").request(MediaType.TEXT_PLAIN).get(String.class);
+				respuesta = target.path("Servidor/pedirTiempo").request(MediaType.TEXT_PLAIN).get(String.class);
 				
 				t3 = System.nanoTime();
 				
@@ -75,14 +83,45 @@ public class Cliente {
 					System.out.println("\n\nError 2: No se pudo convertir el valor de pedirTiempo del servidor " + IP.get(i)
 							+ "\nEn la iteracióon " +j);
 				}  
+				
+				long o = determinarOffset(t0,t1,t2,t3);
+				long d = determinarDelay(t0,t1,t2,t3);
+				
+				pares.add(new Par(o,d));
 
 				//if()	si ( d < mejorPar.d ) mejorPar <- {d,o}
 				
-			
+				// Version sin Marzullo
+				if(d<mejorPar.delay) {
+					mejorPar = new Par(o,d);
+				}
+				////////////
 
 			}
+			
+				System.out.println("Mejor par sin aplicar marzullo: Offset: "+mejorPar.offset+" Delay: "+ mejorPar.delay);
+				// Version con Marzullo
+				mejorPar = marzullo(pares);
+				System.out.println("Mejor par aplicando marzullo: Offset: "+mejorPar.offset+" Delay: "+ mejorPar.delay);
+				////////////
+				
+				
 		}
 		
 		return;
 	}
+	
+	static long determinarOffset(long t0, long t1, long t2, long t3) {
+		return (t1-t0 + t2-t3)/2;
+	}
+	static long determinarDelay(long t0, long t1, long t2, long t3) {
+		return t1-t0 + t3-t2;
+	}
+	static Par marzullo(ArrayList<Par> pares) {
+		int mejor = 0, contador = 0;
+		
+		
+		return new Par(0,0);
+	}
+	
 }
